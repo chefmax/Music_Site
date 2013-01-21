0befcf4558678f04c65ac1e7494b0577f18a843a
chef <chefmax7@gmail.com>
2013-01-21 20:45:17 +0400

    --amend

--- index.py
+++ index.py
@@ -76,9 +76,9 @@ def index(req):
         for i in range(len(parameters)):
             parameters[i] = parameters[i].replace("%20"," ")
             parameters[i] = parameters[i].replace("%21","!")
-            parameters[i] = parameters[i].replace("+"," ")
+            parameters[i] = parameters[i].replace("+"," ") 
         result = getresult(req, parameters)
-
+    return str(parameters)    
     return result
 
 print sys.path        
